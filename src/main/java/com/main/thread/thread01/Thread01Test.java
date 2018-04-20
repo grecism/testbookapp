c4b1df9b600ae72ca2c177e024a5d493e760de86
lulu.zou <lulu.zou@ucarinc.com>
2018-04-20 16:17:24 +0800

    update test

--- src/main/java/com/main/thread/thread01/Thread01Test.java
+++ src/main/java/com/main/thread/thread01/Thread01Test.java
@@ -10,9 +10,17 @@ package com.main.thread.thread01;
  */
 public class Thread01Test {
 	public static void main(String[] args)  {
+		//1.random() 方法可返回介于 0 ~ 1 之间的一个随机数。
+		double a = Math.random();
+		System.out.println("a="+a);
+		double b= a*10000;
+		System.out.println("a*10000="+b);
+		int c =(int)b;
+		System.out.println("int(a*10000)="+c);
 		//System.out.println(Math.random());
-		System.out.println((int)(Math.random()*10000));	
-		System.out.println((int)(Math.random()*1000));	
+		//System.out.println((int)(Math.random()*10000));
+		//System.out.println((int)(Math.random()*1000));
+		//2.SimpleDateFormat
 		//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		//Date date = new Date();
 		//System.out.println(sdf.format(date));
